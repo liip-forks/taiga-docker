9cde696a5a045f90bfd7f2d9d4af1fac58295ea9
Didier 'OdyX' Raboud <didier.raboud@liip.ch>
2023-01-30 17:54:38 +0100

    Allow ENABLE_SLACK customization

--- taiga-back/env-setting-entrypoint.sh
+++ taiga-back/env-setting-entrypoint.sh
@@ -53,6 +53,7 @@ force_python_bool PUBLIC_REGISTER_ENABLED
 force_python_bool EMAIL_USE_TLS
 force_python_bool EMAIL_USE_SSL
 force_python_bool ENABLE_OPENID
+force_python_bool ENABLE_SLACK
 
 # If arguments start with manage.py, launch this first
 if [ "`echo -n $@ | head -c11`" = "./manage.py" ]; then
